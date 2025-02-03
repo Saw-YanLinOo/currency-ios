75bd90697405358fdb6bdbe5aedde8e50c1376b9
YanYan Chan <yly80103@gmail.com>
2025-02-04 00:55:29 +0700

    Initial Commit

---
+++ yccurrency/yccurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  yccurrencyApp.swift
+//  yccurrency
+//
+//  Created by mac on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct yccurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
